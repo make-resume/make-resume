0ba392b4588a991d0b1c461840b272b958b4d292
Inam Ul Huq <inambe.dev@gmail.com>
2020-06-05 18:39:13 +0500

    theme selection (local, built-in) + little theme dir validation

--- bin/models/jtcCli.js
+++ bin/models/jtcCli.js
@@ -1,23 +1,27 @@
 const JsonToCV = require("./../../lib/models/jsonToCV");
 const Message = require("./message");
+const Process = require("./process");
 
 class JtcCli {
 	constructor(opts) {
-		this.dir = opts.dir;
-		this.theme = opts.theme;
-		this.jtc = new JsonToCV({
-			dir: this.dir,
-			theme: this.theme,
-		});
+		this.opts = opts;
+		try {
+			this.jtc = new JsonToCV({
+				dir: this.opts.dir,
+				theme: this.opts.theme,
+			});
+		} catch (e) {
+			Process.exitWithError(e.message);
+		}
 	}
 	async build() {
 		try {
 			await this.jtc.validateDir();
 			await this.jtc.validateInfo();
+			await this.jtc.build();
 		} catch (e) {
 			Process.exitWithError(e.message);
 		}
-		await this.jtc.build();
 		Message.info("project: built");
 	}
 }

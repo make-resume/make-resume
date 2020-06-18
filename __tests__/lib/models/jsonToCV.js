acf224a6070a8db797d0e89d955dd91eab4318d3
Inam Ul Huq <inambe.dev@gmail.com>
2020-06-19 02:35:47 +0500

    added more tests

--- __tests__/lib/models/jsonToCV.js
+++ __tests__/lib/models/jsonToCV.js
@@ -1,13 +1,16 @@
 const path = require("path");
-const { existsSync, mkdirSync } = require("fs");
-const util = require("util");
+const { existsSync, mkdirSync, copyFileSync } = require("fs");
+const { promisify } = require("util");
 const _rimraf = require("rimraf");
 
 const JsonToCv = require("../../../lib/models/jsonToCV");
 
-const rimraf = util.promisify(_rimraf);
+const rimraf = promisify(_rimraf);
 
-const outputFixture = path.join(__dirname, "../../../fixture");
+const outputFixture = path.join(__dirname, "./../../fixture");
+const validTheme = path.join(__dirname, "./../../../lib/themes/basic");
+const infoFileName = "person.json";
+const validInfoFile = path.join(validTheme, infoFileName);
 
 beforeAll(() => {
 	if (!existsSync(outputFixture)) {
@@ -16,12 +19,45 @@ beforeAll(() => {
 });
 
 test("should select a built-in theme", () => {
-	const opts = { theme: "basic", dir: process.cwd() };
+	const opts = { theme: "basic", dir: outputFixture };
 	const jtc = new JsonToCv(opts);
 	expect(jtc.paths.theme).toBe(
 		path.join(__dirname, "../../../lib/themes", opts.theme)
 	);
 	expect(jtc.themeOrigin).toBe("built-in");
+});
+
+test("should throw if directory is not valid", async () => {
+	const opts = {
+		theme: "basic",
+		dir: path.join(outputFixture, "examples/1"),
+	};
+	const jtc = new JsonToCv(opts);
+	await expect(jtc.validateDir()).rejects.toThrow(/does not exist/);
+});
+
+test("should throw if info is not valid", async () => {
+	const opts = {
+		theme: "basic",
+		dir: path.join(outputFixture, "examples/2"),
+	};
+	const jtc = new JsonToCv(opts);
+	await expect(jtc.validateDir()).resolves.toBeUndefined();
+	await expect(jtc.validateInfo()).rejects.toThrow();
+});
+
+test("should build the project", async () => {
+	const dir = path.join(outputFixture, "examples/3");
+	copyFileSync(validInfoFile, path.join(dir, infoFileName));
+	const opts = {
+		theme: "basic",
+		dir: dir,
+	};
+	const jtc = new JsonToCv(opts);
+	await expect(jtc.validateDir()).resolves.toBeUndefined();
+	await expect(jtc.validateInfo()).resolves.toBeUndefined();
+	await expect(jtc.build()).resolves.toBeUndefined();
+	rimraf(path.join(dir, "**/*"));
 });
 
 describe("theme cloning", () => {
